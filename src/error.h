0b1040c605bcfd04c033724763102ecd2ac23480
43741 <hezining@sjtu.edu.cn>
2024-12-22 19:49:07 +0800

    restart the project

---
+++ src/error.h
@@ -0,0 +1,8 @@
+//
+// Created by 43741 on 2024/12/22.
+//
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#endif //ERROR_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
 //
 // Created by 43741 on 2024/12/22.
 //
+#include <exception>
+#include <iostream>
+
+#include "AccountManager.h"
+#include "CommandParser.h"
+#include "BookManager.h"
