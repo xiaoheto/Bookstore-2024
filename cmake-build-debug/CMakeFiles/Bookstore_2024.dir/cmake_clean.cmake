841685c6bb7ab06acdbed23f15418b2742b95df6
43741 <hezining@sjtu.edu.cn>
2024-12-22 19:45:53 +0800

    restart the project

---
+++ cmake-build-debug/CMakeFiles/Bookstore_2024.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Bookstore_2024"
+  "Bookstore_2024.pdb"
+  "CMakeFiles/Bookstore_2024.dir/src/AccountManager.cpp.o"
+  "CMakeFiles/Bookstore_2024.dir/src/AccountManager.cpp.o.d"
+  "CMakeFiles/Bookstore_2024.dir/src/BookManager.cpp.o"
+  "CMakeFiles/Bookstore_2024.dir/src/BookManager.cpp.o.d"
+  "CMakeFiles/Bookstore_2024.dir/src/CommandParser.cpp.o"
+  "CMakeFiles/Bookstore_2024.dir/src/CommandParser.cpp.o.d"
+  "CMakeFiles/Bookstore_2024.dir/src/LogManager.cpp.o"
+  "CMakeFiles/Bookstore_2024.dir/src/LogManager.cpp.o.d"
+  "CMakeFiles/Bookstore_2024.dir/src/main.cpp.o"
+  "CMakeFiles/Bookstore_2024.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bookstore_2024.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
