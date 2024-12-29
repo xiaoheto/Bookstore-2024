94ae30921b47ca0e91af3f8f9e6428eac3ad6074
何子宁 <hezining@sjtu.edu.cn>
2024-12-29 22:36:35 +0800

    Update LogManager.cpp

--- src/LogManager.cpp
+++ src/LogManager.cpp
@@ -5,20 +5,27 @@ LogManager::LogManager() {
 
 void LogManager::ShowFinance(int need) {
     if (need == 0) {
-        std::cout << '\n';
+        printf("\n");
+        return;
     }
-    if (need == -1) {
-        need = financeCount;
+
+    logStorage.get_info(financeCount, 1);
+    if (need > financeCount || need > 2147483647 || financeCount > 2147483647) {
+        throw Error("Invalid\n");
     }
-    double income = 0.0,expense = 0.0;
-    for (int i = financeCount - need + 1;i <= financeCount;++i) {
-        Log temp;
+    if (need == -1) {need = financeCount;}
+
+    double inc = 0, dec = 0;
+    for (int i = financeCount - need + 1; i <= financeCount; ++i){
+        class Log temp;
         logStorage.read(temp, 8 + (i - 1) * sizeof(Log));
-        if (temp.isIncome) income += temp.Amount;
-        else expense += temp.Amount;
+
+        if (temp.isIncome) inc += temp.Amount;
+        else dec += temp.Amount;
     }
-    std::cout << "+ " << std::fixed << std::setprecision(2) << income
-         << " - " << std::fixed << std::setprecision(2) << expense << "\n";
+
+    cout << "+ " << fixed << setprecision(2) << inc
+         << " - " << fixed << setprecision(2) << dec << "\n";
 }
 
 void LogManager::AddLog(Log &log) {
