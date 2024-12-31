b400de3add2826dc1511749ba00005a9d11c1442
43741 <hezining@sjtu.edu.cn>
2025-01-01 04:03:50 +0800

    Book and Account

---
+++ cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/43741/Desktop/Bookstore-2024实现/src/AccountManager.cpp" "CMakeFiles/code.dir/src/AccountManager.cpp.o" "gcc" "CMakeFiles/code.dir/src/AccountManager.cpp.o.d"
+  "/mnt/c/Users/43741/Desktop/Bookstore-2024实现/src/BookManager.cpp" "CMakeFiles/code.dir/src/BookManager.cpp.o" "gcc" "CMakeFiles/code.dir/src/BookManager.cpp.o.d"
+  "/mnt/c/Users/43741/Desktop/Bookstore-2024实现/src/CommandParser.cpp" "CMakeFiles/code.dir/src/CommandParser.cpp.o" "gcc" "CMakeFiles/code.dir/src/CommandParser.cpp.o.d"
+  "/mnt/c/Users/43741/Desktop/Bookstore-2024实现/src/LogManager.cpp" "CMakeFiles/code.dir/src/LogManager.cpp.o" "gcc" "CMakeFiles/code.dir/src/LogManager.cpp.o.d"
+  "/mnt/c/Users/43741/Desktop/Bookstore-2024实现/src/main.cpp" "CMakeFiles/code.dir/src/main.cpp.o" "gcc" "CMakeFiles/code.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/src/AccountManager.cpp.o"
+  "CMakeFiles/code.dir/src/AccountManager.cpp.o.d"
+  "CMakeFiles/code.dir/src/BookManager.cpp.o"
+  "CMakeFiles/code.dir/src/BookManager.cpp.o.d"
+  "CMakeFiles/code.dir/src/CommandParser.cpp.o"
+  "CMakeFiles/code.dir/src/CommandParser.cpp.o.d"
+  "CMakeFiles/code.dir/src/LogManager.cpp.o"
+  "CMakeFiles/code.dir/src/LogManager.cpp.o.d"
+  "CMakeFiles/code.dir/src/main.cpp.o"
+  "CMakeFiles/code.dir/src/main.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
