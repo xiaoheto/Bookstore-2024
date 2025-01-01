6d0901b5d8322eab673547af8390e397746f5e6a
何子宁 <hezining@sjtu.edu.cn>
2025-01-01 20:12:34 +0800

    Update LogManager.cpp

--- src/LogManager.cpp
+++ src/LogManager.cpp
@@ -3,7 +3,8 @@ LogManager::LogManager() {
     logStorage.initialise("log_Storage");
 }
 
-void LogManager::ShowFinance(int need) {
+// LogManager.cpp
+void LogManager::ShowFinance(int need) {  // 删除这里的默认参数
     if (need == 0) {
         std::cout << '\n';
         return;
@@ -16,21 +17,21 @@ void LogManager::ShowFinance(int need) {
         return;
     }
 
-    if (need > financeCount) {
-        throw Error("Invalid\n");
-    }
-
     if (need == -1) {
         need = financeCount;
+    }
+    else if (need > financeCount) {
+        throw Error("Invalid\n");
     }
 
     double inc = 0, dec = 0;
 
+    // 改为用固定值 2
     const int headerSize = 2 * sizeof(int);
-
     for (int i = financeCount - need; i < financeCount; i++) {
         Log temp;
-        logStorage.read(temp, headerSize + i * sizeof(Log));
+        int pos = headerSize + i * sizeof(Log);
+        logStorage.read(temp, pos);
 
         if (temp.behavoir == ActionType::BUY) {
             inc += temp.Amount;
@@ -47,7 +48,10 @@ void LogManager::ShowFinance(int need) {
 void LogManager::AddLog(Log &log) {
     if (log.behavoir == ActionType::BUY || log.behavoir == ActionType::IMPORTBOOK) {
         financeCount++;
+        int pos = 2 * sizeof(int) + (financeCount - 1) * sizeof(Log);
+        logStorage.update(log, pos);  // 使用update到确定位置
         logStorage.write_info(financeCount, 1);
+    } else {
+        logStorage.write(log);  // 只有非财务日志才使用write
     }
-    logStorage.write(log);
 }
